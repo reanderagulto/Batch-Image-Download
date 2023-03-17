0a40585b43a1429625c7b6150e0964946055a663
Reander Agulto <reanderagulto29@gmail.com>
2023-03-17 08:49:26 +0800

    Added data.js

---
+++ data.js
@@ -0,0 +1,22 @@
+export const images = [
+    {
+        fileName: 'real-trends.png',
+        url: "https://resources.agentimage.com/redesign/uploads/2022/11/29080122/20aa038b48651b690e544c9cce47479c.png"
+    },
+    {
+        fileName: 'horizon.png',
+        url: "https://resources.agentimage.com/redesign/uploads/2022/11/29080131/95c08a4fe4c8de4b2ee1e283b0920470.png"
+    },
+    {
+        fileName: 'vega.png',
+        url: "https://resources.agentimage.com/redesign/uploads/2022/11/29080143/855575b68c862b76e88495d221f34890.png"
+    },
+    {
+        fileName: 'hermes.png',
+        url: "https://resources.agentimage.com/redesign/uploads/2022/11/29080152/dc3bd11be864ef492e083c4d554bb916.png"
+    }, 
+    {
+        fileName: 'muse.png',
+        url: "https://resources.agentimage.com/redesign/uploads/2022/11/29080202/07d502793b5a6376cb2358fb060dbb8f.png"
+    },
+]
