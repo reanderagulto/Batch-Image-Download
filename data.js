898ef7b530bb23e14796128f3dc851214d3eaad9
Reander Agulto <reanderagulto29@gmail.com>
2023-03-21 09:38:16 +0800

    Updated .gitignore

--- data.js
+++ data.js
@@ -1,22 +1,102 @@
 export const images = [
     {
-        fileName: 'real-trends.png',
-        url: "https://resources.agentimage.com/redesign/uploads/2022/11/29080122/20aa038b48651b690e544c9cce47479c.png"
+        fileName: 'berkshire-hathaway-logo.png',
+        url: "https://resources.agentimage.com/redesign/uploads/2023/01/11062727/8bf775f0d7683fed744e3e03571ff950.png"
     },
     {
-        fileName: 'horizon.png',
-        url: "https://resources.agentimage.com/redesign/uploads/2022/11/29080131/95c08a4fe4c8de4b2ee1e283b0920470.png"
+        fileName: 'berkshire-hathaway-logo-mono.png',
+        url: "https://resources.agentimage.com/redesign/uploads/2023/01/11062725/b297f369ff161d7364a2d4ec520c8281.png"
     },
     {
-        fileName: 'vega.png',
-        url: "https://resources.agentimage.com/redesign/uploads/2022/11/29080143/855575b68c862b76e88495d221f34890.png"
+        fileName: 'banner-laptop.jpg',
+        url: "https://resources.agentimage.com/redesign/uploads/2023/01/18225105/34cc57f82e60c016ead3b403542b0a9b.jpg"
     },
     {
-        fileName: 'hermes.png',
-        url: "https://resources.agentimage.com/redesign/uploads/2022/11/29080152/dc3bd11be864ef492e083c4d554bb916.png"
+        fileName: 'banner-tablet.jpg',
+        url: "https://resources.agentimage.com/redesign/uploads/2023/01/18225142/3d18e26e80b3488ae7f3d0131f6a337f.jpg"
     }, 
     {
-        fileName: 'muse.png',
-        url: "https://resources.agentimage.com/redesign/uploads/2022/11/29080202/07d502793b5a6376cb2358fb060dbb8f.png"
+        fileName: 'banner-mobile.jpg',
+        url: "https://resources.agentimage.com/redesign/uploads/2023/01/18225108/eaa1c2ee7185e0fdd3d185f5ebb96dbd.jpg"
+    },
+    {
+        fileName: 'uhrig-tablet.jpg',
+        url: "https://resources.agentimage.com/redesign/uploads/2023/01/12024145/caebccd64949d8d11eb41261f3f1437b.jpg"
+    },
+    {
+        fileName: 'uhrig-mobile.jpg',
+        url: "https://resources.agentimage.com/redesign/uploads/2023/01/12065939/4b54edc51d93a52a13bfb2548b86078a.jpg"
+    },
+    {
+        fileName: 'covington-tablet.jpg',
+        url: "https://resources.agentimage.com/redesign/uploads/2023/01/13061400/c53aa8fade5bba6e501426c8e3ebd4e6.jpg"
+    },
+    {
+        fileName: 'covington-mobile.jpg',
+        url: "https://resources.agentimage.com/redesign/uploads/2023/01/13061357/5495f43f430f52baa5a11bf11328a838.jpg"
+    },
+    {
+        fileName: 'gu-tablet.jpg',
+        url: "https://resources.agentimage.com/redesign/uploads/2023/01/13063006/55e10fa242be1c79c1d813ab06b2f149.jpg"
+    },
+    {
+        fileName: 'gu-mobile.jpg',
+        url: "https://resources.agentimage.com/redesign/uploads/2023/01/13063009/4d8fc8bf1fc93abbe2acf01d703c9b77.jpg"
+    },
+    {
+        fileName: 'sheradsky-tablet.jpg',
+        url: "https://resources.agentimage.com/redesign/uploads/2023/01/13064214/e4543d68b8aadb33c2ceed893c2d7938.jpg"
+    },
+    {
+        fileName: 'sheradsky-mobile.jpg',
+        url: "https://resources.agentimage.com/redesign/uploads/2023/01/13064212/2f9c7f5807b73e61d4850c8d226c4abc.jpg"
+    },
+    {
+        fileName: 'gobbell-tablet.jpg',
+        url: "https://resources.agentimage.com/redesign/uploads/2023/01/13064805/0b1a51e2ee3280b7f320226ee7872c3c.jpg"
+    },
+    {
+        fileName: 'gobbell-mobile.jpg',
+        url: "https://resources.agentimage.com/redesign/uploads/2023/01/13064802/99edb25c5a77f51987aa04d0c07e1ff0.jpg"
+    },
+    {
+        fileName: 'cordova-tablet.jpg',
+        url: "https://resources.agentimage.com/redesign/uploads/2023/01/13064922/1ea779a0b6ca221e79eed267de9c2621.jpg"
+    },
+    {
+        fileName: 'cordova-mobile.jpg',
+        url: "https://resources.agentimage.com/redesign/uploads/2023/01/13064925/300f2f71700d0af2f3a3f6183c97fd9e.jpg"
+    },
+    {
+        fileName: 'james-tablet.jpg',
+        url: "https://resources.agentimage.com/redesign/uploads/2023/01/18225105/34cc57f82e60c016ead3b403542b0a9b.jpg"
+    },
+    {
+        fileName: 'james-mobile.jpg',
+        url: "https://resources.agentimage.com/redesign/uploads/2023/01/18225108/eaa1c2ee7185e0fdd3d185f5ebb96dbd.jpg"
+    },
+    {
+        fileName: 'noona-tablet.jpg',
+        url: "https://resources.agentimage.com/redesign/uploads/2023/01/13065743/dd9f6e7c8d29e27600dd36379d922d44.jpg"
+    },
+    {
+        fileName: 'noona-mobile.jpg',
+        url: "https://resources.agentimage.com/redesign/uploads/2023/01/13065746/03ae5cbb3aeedf54d9ce7613a4bb2835.jpg"
+    },
+    {
+        fileName: 'cabral-tablet.jpg',
+        url: "https://resources.agentimage.com/redesign/uploads/2023/01/13070043/f4d355731ca8ada2e13715bd9a520327.jpg"
+    },
+    {
+        fileName: 'cabral-mobile.jpg',
+        url: "https://resources.agentimage.com/redesign/uploads/2023/01/13070046/a5b31170ca33850886c2aa2788e4b384.jpg"
+    },
+    {
+        fileName: 'smith-tablet.jpg',
+        url: "https://resources.agentimage.com/redesign/uploads/2023/01/13070225/f6428f5f472cfc04008bfd02ddcddbaa.jpg"
+    },
+    {
+        fileName: 'smith-mobile.jpg',
+        url: "https://resources.agentimage.com/redesign/uploads/2023/01/13070223/83eedba242068324a621dcaa771189ba.jpg"
     },
 ]
